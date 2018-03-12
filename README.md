2249ebf0d9a5c1f605b41b65214a1c9cf5286237
Iaroslav Zeigerman <zeigerman.ia@gmail.com>
2018-03-12 08:46:22 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Custom themes and plugins which I use together with [oh-my-zsh](https://github.com/robbyrussell/oh-my-zsh). Perhaps when this repo has enough content I'd introduce a proper README. Just copy the content of this repo to the `$ZSH_CUSTOM` directory (`~/.oh-my-zsh/custom/` by default).
---
+++ plugins/find_branch/find_branch.plugin.zsh
@@ -0,0 +1,26 @@
+
+find_branch() {
+  original_dir="$(pwd)"
+
+  pattern=".*"
+  if [[ $# -gt 0 ]]; then
+    pattern="$1"
+  fi
+
+  find_dir="."
+  if [[ $# -gt 1 ]]; then
+    find_dir="$2"
+  fi
+
+  cd $find_dir
+  for d in $(ls "."); do
+    repo_dir="./${d}"
+    if [[ -d "$repo_dir" ]]; then
+      cd $repo_dir
+      test "$(git status 2> /dev/null | grep $pattern)" && print -P "%F{white}${d%/}%f: %F{green}$(git status | grep --color=never  On | awk -F ' ' '{ print $3 }')%f"
+      cd ..
+    fi
+  done
+
+  cd $original_dir
+}
